ed6b7a1207e064ff9dd3914a9d03e2c81d425f7f
Sushma Gowda <110340352+SushmaGowda7@users.noreply.github.com>
2022-09-03 01:54:14 +0530

    edit functionality has added

--- appointmentApp/index.js
+++ appointmentApp/index.js
@@ -1,15 +1,15 @@
 function saveTolocalStorage(event)
 {
     event.preventDefault();
-    const name = event.target.username.value;
     const email = event.target.email.value;
+    const username = event.target.username.value;
     const phone = event.target.phone.value;
-    const object = {
-        name, 
+    const object = { 
         email, 
+        username,
         phone
     }
-    axios.post('https://crudcrud.com/api/32d24bfcdbbb44acbe85b757cc643c5c/AppData', object)
+    axios.post('https://crudcrud.com/api/c7e36e6da94a4e02bdad222e5d7b3bf2/AppData', object)
     .then((response) => {
         showNewUsers(response.data)
     })
@@ -18,7 +18,7 @@ function saveTolocalStorage(event)
         console.log(error)})
 }
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get('https://crudcrud.com/api/32d24bfcdbbb44acbe85b757cc643c5c/AppData')
+    axios.get('https://crudcrud.com/api/c7e36e6da94a4e02bdad222e5d7b3bf2/AppData')
     .then((response) => {
         console.log(response)
 
@@ -33,31 +33,35 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function showNewUsers(user)
 {
-    document.getElementById('username').value = '';
     document.getElementById('email').value = '';
+    document.getElementById('username').value = '';
     document.getElementById('phone').value = '';
 
     if(localStorage.getItem(user.email)!== null){
         removeUserFromScreen(user.email);
     }
     const parentNode = document.getElementById('listOfUsers');
-    const childHtml = `<li id=${user._id}> ${user.name} - ${user.email}
-                            <button onclick = deleteUser('${user._id}')> Delete </button>
-                            <button onclick = editUserDetails('${user._id}','${user.username}', '${user.email}')> Edit </button>
-                        </li>`;
+    const childHtml = `<li id=${user._id}>${user.username} - ${user.email}
+                            <button onclick=deleteUser('${user._id}')>Delete</button>
+                            <button onclick=editUserDetails('${user.email}','${user.username}','${user.phone}','${user._id}')>Edit</button>
+                        </li>`
     parentNode.innerHTML =  parentNode.innerHTML + childHtml;
 }
 
-function editUserDetails(username, email, phone)
+function editUserDetails(email, username, phone, userId)
 {
-    document.getElementById('username').value = username;
-    document.getElementById('email').value = email;
-    document.getElementById('phone').value = phone;
-    deleteUser(email)
+    axios.put(`https://crudcrud.com/api/c7e36e6da94a4e02bdad222e5d7b3bf2/AppData/${userId}`)
+    .then((response) => {
+        document.getElementById('email').value = email;
+        document.getElementById('username').value = username;
+        document.getElementById('phone').value = phone;
+        deleteUser(userId)
+    })
+    .catch((error) => console.log(error))
 }
 
 function deleteUser(userId){
-    axios.delete(`https://crudcrud.com/api/32d24bfcdbbb44acbe85b757cc643c5c/AppData/${userId}`)
+    axios.delete(`https://crudcrud.com/api/c7e36e6da94a4e02bdad222e5d7b3bf2/AppData/${userId}`)
     .then((response) => {
         removeUserFromScreen(userId)
     })
